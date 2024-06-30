b5117e4d0ba252a1e257faf2f786f3174ad54e0c
pedrocacintra <pedrocacintra@gmail.com>
2024-06-30 20:11:08 -0300

    Update ttGetCampaign.php
    Fixed an error where the script would only get costs for one tiktok campaign at a time.

--- ttGetCampaign.php
+++ ttGetCampaign.php
@@ -1,43 +1,47 @@
 <?php
-
-date_default_timezone_set('Europe/Moscow'); // Часовой пояс для ТТ
-
+ 
+date_default_timezone_set('America/New_York'); // Часовой пояс для ТТ
+ 
 include __DIR__.'/functions.php';
 include __DIR__.'/kt_load.php';
 include __DIR__.'/include/db.php';
-
+ 
 $today = date("Y-m-d"); // Сегодняшняя дата за которую запрашивать расход
 echo 'Если ниже вы видите названия ваших кампаний то скрипт работает правильно';
-
-
-$tokens = $db->query("SELECT * FROM `tokens`  ORDER BY id DESC");
-
+ 
+$tokens = $db->query("SELECT * FROM `tokens` ORDER BY id DESC");
+ 
 foreach ($tokens as $token) {
     $sessionid_ss_ads = $token['sessionid_ss_ads'];
     $csrfToken = $token['csrf'];
     $idAccount = $token['id_ad_account'];
     $today = date("Y-m-d"); // Сегодняшняя дата за которую запрашивать расход
-    $getStatisticsAll = getStatisticsAll($idAccount, $csrfToken, $sessionid_ss_ads, $today); //
-
-
-    /**
-     * Оставляем только расход
-     */
-
+    $getStatisticsAll = getStatisticsAll($idAccount, $csrfToken, $sessionid_ss_ads, $today);
+ 
+    $costs = []; // Array to hold aggregated costs
+ 
     foreach ($getStatisticsAll['data']['table'] as $taskValue) {
-
-        $re = '/(?<=\{).*(?=})/m';
+        $re = '/(?<=\\{).*(?=})/m';
         $id_campaign_keitaro = $taskValue['campaign_name'];
         preg_match_all($re, $id_campaign_keitaro, $matches, PREG_SET_ORDER, 0);
-
-        /**
-         * Выводим только те кампании где есть макрос {id}
-         */
+ 
+        // Выводим только те кампании где есть макрос {id}
         if (isset($matches[0][0])) {
             $id_campaign_keitaro = $matches[0][0];
-
-            echo "<br>Кампания: (" . $id_campaign_keitaro . ") " . $taskValue['campaign_name'] . " Расход: " . $taskValue['stat_data']['stat_cost'] . '<br>';
-            echo sendCostKeitaro($taskValue['stat_data']['stat_cost'], $id_campaign_keitaro, $today);
+            $cost = $taskValue['stat_data']['stat_cost'];
+ 
+            if (isset($costs[$id_campaign_keitaro])) {
+                $costs[$id_campaign_keitaro] += $cost;
+            } else {
+                $costs[$id_campaign_keitaro] = $cost;
+            }
+ 
+            echo "<br>Кампания: (" . $id_campaign_keitaro . ") " . $taskValue['campaign_name'] . " Расход: " . $cost . '<br>';
         }
+    }
+ 
+    // Send aggregated costs to Keitaro
+    foreach ($costs as $id_campaign_keitaro => $total_cost) {
+        echo sendCostKeitaro($total_cost, $id_campaign_keitaro, $today);
     }
 }
